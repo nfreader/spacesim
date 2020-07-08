394c704def2555789ceac64934f826bba54defa7
Nick <nick@nfreader.net>
2020-07-07 20:15:57 -0400

    Adds more methods to flash messages

--- src/Notification/Flash.php
+++ src/Notification/Flash.php
@@ -5,13 +5,36 @@ namespace ssim\Notification;
 class Flash {
 
   public function __construct(){
+    
   }
 
-  public function Success(string $text) {
+  private function addMessage(
+    string $text = 'This is a message', 
+    string $level = 'info',
+    int $code = 200) {
     $message = new \stdclass;
     $message->text = $text;
-    $message->type = 'success';
+    $message->type = $level;
+    $message->code = $code;
     $_SESSION[SSIM_IDENT]['flash'][] = $message;
+  }
+
+  public function Success(string $text = 'This is a success message') {
+    $this->addMessage($text, 'success');
+  }
+
+  public function Info(string $text = 'This is an info message') {
+    $this->addMessage($text, 'info');
+  }
+
+  public function Warning(string $text = 'This is a warning message') {
+    $this->addMessage($text, 'warning');
+  }
+
+  public function Error(
+    string $text = 'This is an error message',
+    int $code = 500) {
+    $this->addMessage($text, 'danger', $code);
   }
 
 }
