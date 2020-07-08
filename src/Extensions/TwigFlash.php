fe6cf263d8710b14708a057337d6431bf6551a6a
Nick <nick@nfreader.net>
2020-07-07 20:13:44 -0400

    Cleaning up flash messages

--- src/Extensions/TwigFlash.php
+++ src/Extensions/TwigFlash.php
@@ -5,8 +5,10 @@ namespace ssim\Extensions;
 class TwigFlash extends \Twig\Extension\AbstractExtension implements \Twig\Extension\GlobalsInterface {
 
   public function getGlobals(): array {
+    $flash = $_SESSION[SSIM_IDENT]['flash'];
+    $_SESSION[SSIM_IDENT]['flash'] = [];
     return [
-      'flash' => $_SESSION[SSIM_IDENT]['flash']
+      'flash' => $flash
     ];
   }
 
