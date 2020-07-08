c617b4d45b1b5ca468411bc84bb504b8e6393575
Nick <nick@nfreader.net>
2020-07-07 20:13:06 -0400

    hasPermission on user model

--- src/Model/User.php
+++ src/Model/User.php
@@ -8,12 +8,20 @@ class User {
 
   public $id = 0;
   public $email = null;
-  public $permissions = false;
+  public $permissions = [];
 
   public function __construct($user) {
     $this->email = $user->email;
     $this->id = $user->id;
     $this->permissions = $user->permissions;
   }
+
+  public function hasPermission(?string $permission = null) {
+    if (in_array($permission, array_keys($this->permissions), TRUE)){
+      return true;
+    }
+    return false;
+  }
+
 
 }
