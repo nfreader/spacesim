dc0503ce910f0baff9b0bfa599812c9962a99174
Nick <nick@nfreader.net>
2020-07-09 22:39:27 -0400

    Removing unncessary code from base Model class

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -2,14 +2,6 @@
 
 namespace ssim\Model;
 
-use Selective\Config\Configuration as Config;
-
 class Model {
-
-  protected $config;
-
-  public function __constuct(Config $config) {
-    $this->config = $config;
-  }
 
 }
