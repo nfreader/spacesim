6317ebf314d395237562ea717d77df5dc396cfc0
Nick <nick@nfreader.net>
2020-07-18 22:34:19 -0400

    Refactoring namespaces

--- app/routes.php
+++ app/routes.php
@@ -4,40 +4,50 @@ use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app){
-  $app->get('/', \ssim\Action\Home::class)->setName('home');
+  // BACKEND 
+  $app->get('/', \ssim\Controllers\Http\Home::class)->setName('home');
 
-  $app->map(['GET', 'POST'],'/game', \ssim\Action\Game\LaunchGame::class)->setName('game');
+  $app->map(['GET', 'POST'],'/game', \ssim\Controllers\Http\Game\LaunchGame::class)->setName('game');
 
   $app->group('/account', function (RouteCollectorProxy $group){
-    $group->post('/register', \ssim\Action\Account\Register::class)->setName('register');
-    $group->post('/login', \ssim\Action\Account\Login::class)->setName('login');
-    $group->get('/logout', \ssim\Action\Account\Logout::class)->setName('logout');
+
+    $group->post('/register', \ssim\Controllers\Http\Account\Register::class)->setName('register');
+
+    $group->post('/login', \ssim\Controllers\Http\Account\Login::class)->setName('login');
+
+    $group->get('/logout', \ssim\Controllers\Http\Account\Logout::class)->setName('logout');
   });
 
   $app->group('/pilots', function (RouteCollectorProxy $group){
-    $group->get('/', \ssim\Action\Pilots\ViewPilots::class)->setName('pilots.view');
-    $group->map(['POST','GET'], '/addCompany', \ssim\Action\Pilots\AddCompany::class)->setName('pilots.addCompany');
-    $group->map(['POST','GET'], '/addPilot', \ssim\Action\Pilots\AddPilot::class)->setName('pilots.addPilot');
+
+    $group->get('/', \ssim\Controllers\Http\Pilots\ViewPilots::class)->setName('pilots.view');
+
+    $group->map(['POST','GET'], '/addCompany', \ssim\Controllers\Http\Pilots\AddCompany::class)->setName('pilots.addCompany');
+
+    $group->map(['POST','GET'], '/addPilot', \ssim\Controllers\Http\Pilots\AddPilot::class)->setName('pilots.addPilot');
+
   });
 
   $app->group('/ships', function (RouteCollectorProxy $group){
-    $group->get('/', \ssim\Action\Ships\ViewShips::class)->setName('ships.view');
-    $group->map(['POST','GET'], '/addNew', \ssim\Action\Ships\AddShip::class)->setName('ships.newShip');
+
+    $group->get('/', \ssim\Controllers\Http\Ships\ViewShips::class)->setName('ships.view');
+
+    $group->map(['POST','GET'], '/addNew', \ssim\Controllers\Http\Ships\AddShip::class)->setName('ships.newShip');
   });
 
   $app->group('/galaxy', function (RouteCollectorProxy $group){
 
-    $group->get('/', \ssim\Action\Galaxy\ViewGalaxy::class)->setName('galaxy.view');
+    $group->get('/', \ssim\Controllers\Http\Galaxy\ViewGalaxy::class)->setName('galaxy.view');
 
-    $group->get('/star/{id}', \ssim\Action\Galaxy\ViewStar::class)->setName('galaxy.star');
+    $group->get('/star/{id}', \ssim\Controllers\Http\Galaxy\ViewStar::class)->setName('galaxy.star');
 
-    $group->map(['POST','GET'], '/addStar', \ssim\Action\Galaxy\AddStar::class)->setName('galaxy.newStar')->setArgument('permission','GALAXY');
+    $group->map(['POST','GET'], '/addStar', \ssim\Controllers\Http\Galaxy\AddStar::class)->setName('galaxy.newStar')->setArgument('permission','GALAXY');
 
-    $group->map(['POST','GET'], '/star/{id}/addSyst', \ssim\Action\Syst\AddSyst::class)->setName('galaxy.newSyst')->setArgument('permission','GALAXY');
+    $group->map(['POST','GET'], '/star/{id}/addSyst', \ssim\Controllers\Http\Syst\AddSyst::class)->setName('galaxy.newSyst')->setArgument('permission','GALAXY');
 
-    $group->map(['POST','GET'], '/star/{starid}/{systid}/addSpob', \ssim\Action\Spob\AddSpob::class)->setName('galaxy.newSpob')->setArgument('permission','GALAXY');
+    $group->map(['POST','GET'], '/star/{starid}/{systid}/addSpob', \ssim\Controllers\Http\Spob\AddSpob::class)->setName('galaxy.newSpob')->setArgument('permission','GALAXY');
 
-    $group->get('/star/{starid}/{systid}', \ssim\Action\Syst\ViewSyst::class)->setName('galaxy.syst');
+    $group->get('/star/{starid}/{systid}', \ssim\Controllers\Http\Syst\ViewSyst::class)->setName('galaxy.syst');
 
   })->add(ssim\Guard\UserGuard::class);
 };
