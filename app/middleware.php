40161eeb305a9108a733072782927545cfd0c6fb
Nick <nick@nfreader.net>
2020-10-22 22:15:47 -0400

    Cleaning up middleware

--- app/middleware.php
+++ app/middleware.php
@@ -2,20 +2,13 @@
 
 use Slim\App;
 use Slim\Views\TwigMiddleware;
-use Slim\Middleware\ErrorMiddleware;
-use App\Middleware\SessionMiddleware;
-use App\Middleware\UrlGeneratorMiddleware;
 use App\Guard\UserGuard;
+use App\Middleware\UrlGeneratorMiddleware;
 
-// use App\Middleware\HttpExceptionMiddleware;
-// use App\Middleware\UserMiddleware;
 return function (App $app) {
   $app->addBodyParsingMiddleware();
   $app->add(UserGuard::class);
-  $app->add(SessionMiddleware::class);
   $app->add(UrlGeneratorMiddleware::class);
   $app->addRoutingMiddleware();
   $app->add(TwigMiddleware::createFromContainer($app, 'view'));
-  // $app->add(HttpExceptionMiddleware::class);
-  // $app->add(ErrorMiddleware::class);
 };
